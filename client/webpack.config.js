ea5b5314516bdf0efa2f98a0742a31d4fa3f291e
Ashleigh Pfander <ashleighharvey@MacBook-Pro.lan>
2024-04-07 22:16:59 -0400

    testing connection on new laptop

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,10 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
@@ -36,11 +39,11 @@ module.exports = () => {
         description: 'Takes notes with Javascript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
